9f9df3118b9e0dbde64f31d54e1d031c6331268c
brrrud <106093165+brrrud@users.noreply.github.com>
2022-10-15 04:08:00 +0300

    Create 2_2.c

---
+++ 2_2.c
@@ -0,0 +1,107 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<ctype.h>
+
+int lencnt(char* str) {
+    int len = 0;
+    while(str[len]!='\0'){
+        len++;
+    }
+    return len;
+}
+char* reverse(char*str, int len){
+    for (int i = 0; i < len/2; i++) {   
+        char temp = str[i];  
+        str[i] = str[len - i - 1];  
+        str[len - i - 1] = temp;  
+    }  
+    return(str);
+}
+char* regU(char* str) { 
+    for(int i = 1; str[i] != '\0'; i+=2){
+        if(str[i] <= 'z' && str[i] >= 'a') {
+            str[i] += 'A' - 'a';
+        }
+    }
+    return str;
+}
+char* concat(char* str, char*cont, int len, int cnt) { 
+    char* concatstr = malloc(len+cnt+1);
+    for(int i = 0; i < len; i++) {
+        concatstr[i] = str[i];
+    } 
+    for(int  i = 0; i < cnt; i++) {
+        concatstr[len + i] = cont[i];
+    } 
+    return concatstr;
+}
+char* sortN(char* str, int len, char* sorted) {
+    int k = 0;
+    for(int i = 0; i < len; ++i) {
+        if (isdigit(str[i])) {
+            sorted[k] = str[i];
+            k++;
+        }
+    }
+    for(int i = 0; i < len; ++i) {
+        if (isalpha(str[i])) {
+            sorted[k] = str[i];
+            k++;
+        }
+    }
+    for(int i = 0; i < len; ++i) {
+        if (!isdigit(str[i]) && !isalpha(str[i])) {
+            sorted[k] = str[i];
+            k++;
+        }
+    }
+    return sorted;
+}
+
+int main(int argc, char* argv[]) {
+    if (argv[2][0] != '-') {
+        printf("Неверные флаги");
+        return 0;
+    }
+    if (lencnt(argv[2]) != 2) {
+        printf("Неверные флаги");
+        return 0;
+    }
+    if (argc < 3) {
+        printf("Недостаточно элементов");
+        return 0;
+    }
+    else {
+        if(argc == 3 && argv[2][1] == 'c'){
+            printf("Не хватает аргумента");
+            return 0;
+        }
+        if(argc > 4){
+            printf("Слишком много аргументов");
+            return 0;
+        }
+    }
+    char* str = argv[1];
+    char flag = argv[2][1];
+    int len = lencnt(str);
+
+    if(flag == 'l') {
+        printf("%d", len);
+    }
+    if(flag == 'r') {
+        printf("%s", reverse(str, len));
+    }
+    if(flag == 'u') {
+        printf("%s", regU(str));
+    }
+    if(flag == 'n') {
+        char sorted[len];
+        printf("%s", sortN(str, len, sorted));
+    }
+    if(flag == 'c') {
+        char* cont = argv[3];
+        int contlen = lencnt(cont);
+        printf("%s", concat(str, cont, len, contlen));
+    }
+    return 0;
+}
